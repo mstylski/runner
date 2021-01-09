45db19cde08473ae5c5bb944edd011cfca14a1df
Michał Stylski <tim@timrich.org>
2021-01-09 20:33:23 +0100

    main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://www.strava.com/api/v3/',
   stravaClientID: '57822',
-  stravaRedirectUrl: 'https://mstylski.github.io/runner'
+  stravaRedirectUrl: 'https://mstylski.github.io/runner/'
 };
